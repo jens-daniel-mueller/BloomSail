7cd025c313458acfe2f7487ff5446379b0a8751d
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-01-08 12:48:42 +0100

    modified figs

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -107,7 +107,7 @@ wflow_build()
 wflow_publish("analysis/*", message = "XXX")
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "use only V2 of Fig S5")
+wflow_publish(all = TRUE, message = "modified figs")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c(
