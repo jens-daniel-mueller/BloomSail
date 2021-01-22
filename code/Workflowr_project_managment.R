a75000d88335dc0e491d9fa175d8206961dfd1a5
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-01-22 15:52:16 +0100

    all figs revised

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -120,7 +120,7 @@ wflow_publish(c(
   "analysis/CT_dynamics.Rmd",
   "analysis/Phytoplankton.Rmd",
   "analysis/NCP_reconstruction.Rmd"
-), message = "new Fig_AX names, A5 added", republish = TRUE)
+), message = "all figs revised", republish = TRUE)
 
 
 # Push latest version to GitHub
