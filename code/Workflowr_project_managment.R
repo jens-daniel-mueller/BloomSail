84101446f827a333b8d98f7555e3569112be0dd8
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-01-29 17:00:07 +0100

    resized appendix figures

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -107,7 +107,7 @@ wflow_build()
 wflow_publish("analysis/*", message = "XXX")
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "renamed appendix figures")
+wflow_publish(all = TRUE, message = "resized appendix figures")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c(
