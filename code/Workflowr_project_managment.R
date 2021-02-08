3cd1aabd7604784cd058078e42130a6055333b29
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-02-08 15:16:35 +0100

    code and output cleaning

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -107,7 +107,7 @@ wflow_build()
 wflow_publish("analysis/*", message = "XXX")
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "resized figures")
+wflow_publish(all = TRUE, message = "code and output cleaning")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c(
