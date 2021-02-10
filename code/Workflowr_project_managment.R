0669cbe4b0c2dac025ab598025f3f0e16bb44de4
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-02-10 13:29:13 +0100

    italic p in pCO2

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -107,7 +107,7 @@ wflow_build()
 wflow_publish("analysis/*", message = "XXX")
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "code and output cleaning")
+wflow_publish(all = TRUE, message = "italic p in pCO2")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c(
